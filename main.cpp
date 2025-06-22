3b1bf159fea178838cc19259e2fa0630262beb98
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2025-06-22 18:16:10 +1000

    added some centered text and added extra todo list

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <ncurses.h>
+#include <string>
 using namespace std;
+void namePrint(){
+	//todo: Create an animation so that 'Valentino Webpage' gets printed out in the center letter by letter. Then the users presses anything on keybaord then show main webpage
+}
 void create_webpage(){
 	//TODO: Create format and basic layout of the Webpage 
 	clear();
+	//Getting dimenstions of terminal 
+	int y ,x;
+	getmaxyx(stdscr, y, x);
+	int center_y = y /2;
+	int center_x = x /2;
+	string tabs = "This is centered text test";
+	mvprintw(center_y, center_x - tabs.length() /2, tabs.c_str());
 	refresh();
 }
 int main(){
