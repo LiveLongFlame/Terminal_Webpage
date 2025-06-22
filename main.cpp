d7c6e5eb39fb31f4129fe2718599cbf657e75563
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2025-06-22 18:01:46 +1000

    initialised cureser screen and cleard terminal

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <ncurses.h>
 using namespace std;
+void create_webpage(){
+	//TODO: Create format and basic layout of the Webpage 
+	clear();
+	refresh();
+}
 int main(){
-	cout << "Hello This is my personal Webpage";
+	//This program uses ncurses to get user input from keyboard and navigate around CLI 
+	//TODO: Add keyboard movements and layouts
+	/* initialize curses */
+	initscr();     
+	noecho();          
+	cbreak();      
+	//hide cursor
+	curs_set(0);   
+
+    create_webpage();
+	// Wait for user input before exiting
+    getch();
+	endwin();      
 	return 0;
 }
