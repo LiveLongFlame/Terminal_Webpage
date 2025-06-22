44af7b5574902474bcfe32fdcef96d16681209e1
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2025-06-22 20:21:19 +1000

    refactored namePrint so now it is in its own function which can be called any time

--- main.cpp
+++ main.cpp
@@ -4,41 +4,37 @@
 #include <unistd.h>
 using namespace std;
 enum Tab { ABOUT, PROJECTS, CONTACT };
-void namePrint(string& tittle , int row, int col, int delay = 100000){
-	//Creates animation for webpage 
-	for(size_t i =0; i < tittle.size();i++){
-		mvaddch(row, col + i, tittle[i]);
-		mvaddch(row +1, col + i, '-');
+void namePrint() {
+    clear();
+    int row, col;
+    getmaxyx(stdscr, row, col);
+
+    std::string tittle = "Valentino's Webpage";
+    int title_row = row / 2;
+    int title_col = (col - tittle.length()) / 2;
+    int delay = 100000; // 100ms
+
+    // Animate the title
+    for (size_t i = 0; i < tittle.size(); i++) {
+        mvaddch(title_row, title_col + i, tittle[i]);
+        mvaddch(title_row + 1, title_col + i, '-');
         refresh();
         usleep(delay);
-	}
-
+    }
 }
+
 void create_webpage(Tab currentTab){
 	clear();
     // Get terminal size
     int rows, cols;
     getmaxyx(stdscr, rows, cols);
-    string title = "Valentino's Webpage";
-    int title_row = rows / 2;
-    int title_col = (cols - title.length()) / 2;
-	//_____________________________________________________________-
-    // Animate the title
-    // namePrint(title, title_row, title_col);
-    // Wait for key press to show next content
-	//usleep(800000);
-	//_____________________________________________________________-
-    clear();
 	//todo: Draw the webpage layout!!!!
-	mvprintw((rows / 2 ) - 8, (cols - title.length()) /2 , title.c_str());	
-    //string tabs = "[a: About Me]  [p: Projects]  [c: Contact]";
-    //mvprintw((rows / 2) - 5, (cols - tabs.length()) / 2, "%s", tabs.c_str());
-
+	
 	// Tabs
-	std::string tab_about = "[a: About Me]";
-	std::string tab_projects = "[p: Projects]";
-	std::string tab_contact = "[c: Contact]";
-	int tabs_row = title_row - 5;
+	string tab_about = "[a: About Me]";
+	string tab_projects = "[p: Projects]";
+	string tab_contact = "[c: Contact]";
+	int tabs_row = (rows / 2) - 5;
 
 	int col_start = (cols - (tab_about.length() + tab_projects.length() + tab_contact.length() + 4)) / 2;
 
@@ -67,6 +63,12 @@ void create_webpage(Tab currentTab){
 		string info = "THIS IS INFORMATION ABOUT ME!!!!!";
 		mvprintw((rows / 2), (cols - info.length()) / 2, "%s", info.c_str());
 	}
+
+
+	//Telling users controls and inputs
+	//q:quit , h/j/k/l or ← ↓ ↑ → Navigation
+	string controls = "[q:quit] [h/j/k/l or < v ^ >  navigate]";
+	mvprintw((rows / 2) + 8, (cols - controls.length()) /2, "%s" , controls.c_str()); 
     refresh();
 }
 int main(){
@@ -78,13 +80,16 @@ int main(){
 	cbreak();      
 	//hide cursor
 	curs_set(0);   
-	//TODO: Adding colours to different tabs
+	//allows for keypad interaction 
+	keypad(stdscr, TRUE);
+	// Adding colours to different tabs
 	start_color();
 	use_default_colors();
 	init_pair(1, COLOR_MAGENTA, -1); // About = Purple
 	init_pair(2, COLOR_CYAN, -1);    // Projects = Light Blue
 	init_pair(3, COLOR_YELLOW, -1);  // Contact = Orange-ish
-
+	namePrint();	
+	usleep(800000);
 	// get user inputs from keyboard
 	Tab currentTab = ABOUT;	
     create_webpage(currentTab);
